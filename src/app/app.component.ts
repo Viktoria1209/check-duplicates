0713413181535dd50da13949832151a73ace7be8
Viktoria Boyarko <goncharenko.viktoria1209@gmail.com>
2021-08-11 19:15:53 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'find-duplicate';
+  data!: string;
+
+  getInputValue(data: string): void {
+    this.data = data;
+    console.log(this.findRepeat(data))
+  }
+
+  findRepeat(data: string) {
+    this.data = data;
+
+    const splittedData = data.split('');
+    const hash = new Map();
+    const result: any[] = [];
+    for (let i = 0; i < splittedData.length; i++) {
+      if (hash.get(splittedData[i]) === undefined) {
+        hash.set(splittedData[i], true);
+      } else {
+        const value = hash.get(splittedData[i]);
+        if (value) {
+          hash.set(splittedData[i], !value);
+        }
+      }
+    }
+    hash.forEach((value, key) => {
+      if (!value)
+        result.push(key);
+    });
+    return result.join('');
+  }
 }
